510476beab250e3b3d597647503d812b80add7f5
Sushil <sushilpatil953825@gmail.com>
2025-09-19 01:26:54 +0530

    Adding build fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,8 @@ import { useState } from "react";
 import Loader from "@/sections/Loader";
 import Content from "@/sections/Content";
 
-import Hero from "@/sections/Hero";
-import Footer from "@/sections/Footer";
+// import Hero from "@/sections/Hero";
+// import Footer from "@/sections/Footer";
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
